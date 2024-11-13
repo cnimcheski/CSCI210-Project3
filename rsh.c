107c86feaf549bcf39aafc776b83d77319811b01
cnimcheski <cnimcheski@hotmail.com>
2024-11-13 14:06:22 -0700

    Added the help command and exit command

--- rsh.c
+++ rsh.c
@@ -15,7 +15,7 @@ int isAllowed(const char*cmd) {
 	// TODO
 	// return 1 if cmd is one of the allowed commands
 	// return 0 otherwise
-	
+	printf("hello");
 	return 0;
 }
 
@@ -26,21 +26,44 @@ int main() {
 
     char line[256];
 
+	// infinite loop till exit command is run...
     while (1) {
 
-	fprintf(stderr,"rsh>");
+		fprintf(stderr,"rsh>");
 
-	if (fgets(line,256,stdin)==NULL) continue;
+		if (fgets(line,256,stdin)==NULL) continue;
 
-	if (strcmp(line,"\n")==0) continue;
+		if (strcmp(line,"\n")==0) continue;
 
-	line[strlen(line)-1]='\0';
+		line[strlen(line)-1]='\0';
 
-	// TODO
-	// Add code to spawn processes for the first 9 commands
-	// And add code to execute cd, exit, help commands
-	// Use the example provided in myspawn.c
+		if (strcmp(line, "exit") == 0) { 
+			break; 
+		}
+
+		if (strcmp(line, "help") == 0) {
+			// print out all the commands...
+			printf("The allowed commands are:\n");
+			printf("1: cp\n");
+			printf("2: touch\n");
+			printf("3: mkdir\n");
+			printf("4: ls\n");
+			printf("5: pwd\n");
+			printf("6: cat\n");
+			printf("7: grep\n");
+			printf("8: chmod\n");
+			printf("9: diff\n");
+			printf("10: cd\n");
+			printf("11: exit\n");
+			printf("12: help\n");
+		}
+
+		// TODO
+		// Add code to spawn processes for the first 9 commands
+		// And add code to execute cd, exit, help commands
+		// Use the example provided in myspawn.c
 
     }
+
     return 0;
 }
